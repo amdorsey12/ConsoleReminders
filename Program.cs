54c4f212e2245741bff03a67533f08b1fd7f366a
Chris Markwardt <cjmarkwardt95@gmail.com>
2020-05-15 12:54:27 -0500

    Reorganized file structure

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleReminders
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var manager = new ReminderManager();
+            manager.Start();
+            
+            await manager.Remind( new Reminder { Content = "First", RemindTime = DateTime.Now.AddSeconds(10), IsDone = false },
+                            new Reminder { Content = "Second", RemindTime = DateTime.Now.AddSeconds(20), IsDone = false },
+                            new Reminder { Content = "Third", RemindTime = DateTime.Now.AddSeconds(30), IsDone = false }
+                        );
+            await Task.Delay(30000);
+            manager.Stop();
+        }
+    }
+}
