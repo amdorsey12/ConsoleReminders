86ae8b3595b8b32059ccb4d6187494d9ea47e0d1
amdorsey12 <dorsey.adrian.adrian@gmail.com>
2020-05-22 18:33:43 -0500

    fix program spacing

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,10 +10,12 @@ namespace Dorsey.Reminders
             using (var manager = new ReminderManager(new ConsoleNotifier(), new LiteDbStore()))
             {
                 manager.Start();
-                manager.Remind( new Reminder { Content = "First", RemindTime = DateTime.Now.AddSeconds(10), IsDone = false },
-                                new Reminder { Content = "Second", RemindTime = DateTime.Now.AddSeconds(20), IsDone = false },
-                                new Reminder { Content = "Third", RemindTime = DateTime.Now.AddSeconds(30), IsDone = false }
-                            );
+                manager.Remind
+                ( 
+                    new Reminder { Content = "First", RemindTime = DateTime.Now.AddSeconds(10), IsDone = false },
+                    new Reminder { Content = "Second", RemindTime = DateTime.Now.AddSeconds(20), IsDone = false },
+                    new Reminder { Content = "Third", RemindTime = DateTime.Now.AddSeconds(30), IsDone = false }
+                );
                 await Task.Delay(35000);
                 manager.Stop();
             }
