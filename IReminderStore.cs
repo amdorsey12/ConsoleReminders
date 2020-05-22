bae82fcee2c662ce82d8e463c19fd55594b0fb09
amdorsey12 <dorsey.adrian.adrian@gmail.com>
2020-05-22 09:30:32 -0500

    clean up

--- IReminderStore.cs
+++ IReminderStore.cs
@@ -15,8 +15,10 @@ namespace Dorsey.Reminders
     {
         public static void Delete(this IReminderStore store, params IReminder[] reminders)
             => store.Delete((IEnumerable<IReminder>)reminders);
+
         public static void Store(this IReminderStore store, params IReminder[] reminders)
-        => store.Store((IEnumerable<IReminder>)reminders);
+            => store.Store((IEnumerable<IReminder>)reminders);
+            
         public static void MarkDone(this IReminderStore store, params IReminder[] reminders)
             => store.MarkDone((IEnumerable<IReminder>)reminders);
     }
